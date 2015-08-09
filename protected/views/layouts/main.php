de726f065b17fa35ae6fe8bf09405b675d1a29b0
moylua <moylua@gmail.com>
2015-08-09 21:54:19 +0800

    init

---
+++ protected/views/layouts/main.php
@@ -0,0 +1,70 @@
+<?php /* @var $this Controller */ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="language" content="en">
+
+	<!-- blueprint CSS framework -->
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection">
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print">
+	<!--[if lt IE 8]>
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection">
+	<![endif]-->
+
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css">
+
+	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+</head>
+<body>
+<div>
+    <?php
+    $this->widget(
+        'booster.widgets.TbNavbar',
+        array(
+            'brand' => 'Doctor App Manager',
+            'fixed' => true,
+            'fluid' => true,
+            'items' => array(
+                array(
+                    'class' => 'booster.widgets.TbMenu',
+                    'type' => 'navbar',
+                    'items' => array(
+                        array('label' => 'Home', 'url' => array('/site/index')),
+                        array('label' => 'About', 'url' => array('/site/page', 'view' => 'about')),
+                        array('label' => 'Contact', 'url' => array('/site/contact')),
+                        array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
+                        array('label' => 'Logout (' . Yii::app()->user->name . ')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest)
+
+                    )
+                )
+            )
+        ));
+    ?>
+</div>
+<div>
+
+	<?php if(isset($this->breadcrumbs)):?>
+		<?php $this->widget(
+            'booster.widgets.TbBreadcrumbs',
+            array(
+                'links' => $this->breadcrumbs
+            )
+        );
+
+	endif?>
+
+	<?php echo $content; ?>
+
+	<div class="clear"></div>
+
+	<div id="footer">
+		Copyright &copy; <?php echo date('Y'); ?> by Team Gummy Bears.<br/>
+		All Rights Reserved.<br/>
+	</div><!-- footer -->
+
+</div><!-- page -->
+
+</body>
+</html>
